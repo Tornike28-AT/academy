840ec257566955604fb76faaf6fe7e85515adf03
tornike kharatishvili <tornikekharatishvili@tornikes-MacBook-Air.local>
2026-07-13 00:04:06 +0400

    second

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Hero from "@/components/Hero";
+import WhyUs from "@/components/WhyUs";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
 
     <Hero/>
+    <WhyUs/>
     
     
     </>
