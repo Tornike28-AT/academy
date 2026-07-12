43158e31af9d0060b5efbb7d31a17476f4e66ab8
tornike kharatishvili <tornikekharatishvili@tornikes-MacBook-Air.local>
2026-07-12 12:39:45 +0400

    first commit

---
+++ components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='w-full h-screen bg-black'>
+       <div>
+           
+       </div>
+    </section>
+  )
+}
+
+export default Hero
