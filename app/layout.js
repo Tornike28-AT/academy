b6bec7ec6cd004291593a00e82af833f19525acc
anri <anriskr14@gmail.com>
2026-07-31 00:50:01 +0400

    google analytics

--- app/layout.js
+++ app/layout.js
@@ -119,6 +119,15 @@ export default function RootLayout({ children }) {
       className={`${geoHakuna.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-56MJ3P9MSF"></script>
+    <script> 
+      window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments) }
+  gtag('js', new Date());
+
+  gtag('config', 'G-56MJ3P9MSF');
+</script>
         {children}
         <Footer />
       </body>
